c8250657c2621ad21c7a300b3e41a584c98ee500
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2025-12-27 22:32:00 +0530

    Create String slicing.py
    works perfectly and shows its length and other things in this slicing method

---
+++ String slicing.py
@@ -0,0 +1,22 @@
+#program on string slicing by taking input from the person
+
+str1=(input("Enter any string value: "))
+print("Your entered value is:",str1)
+print(len(str1))    #shows its length
+print(str1[0])      #shows first letter of your string value
+print(str1[-1])     #shows last letter of your name 
+
+
+#program to convert string into uppper and lower case
+str2=(input("Enter any string value: "))
+print("Your entered value is:",str2)
+print(str2.upper())     #converts to uppercase
+print(str2.lower())     #converts to lowercase
+
+
+#program that shows starting and ending 3 characters of your given string values
+str3=(input("Enter any string value: "))
+print("Your entered value is:",str3)
+print(str3[0:3])
+print(str3[-3:])
+
