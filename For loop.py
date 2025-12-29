04c533e834093dfb82e4b449e34487fedf867f66
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2025-12-29 17:27:03 +0530

    Create For loop.py
    Made with for loop and prints each character of string value , and shows length of  string value without using len() function

---
+++ For loop.py
@@ -0,0 +1,7 @@
+#program using for loop and strings by taking input from user and printing each character in new line and also checking its length without using len() function.
+Input=input("Enter string value:")
+count=0
+for i in Input:
+    print(i)
+    count+=1
+print("Length of string is:",count)
