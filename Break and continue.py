c2eb99e8eb8be7505fc9e2394df5003fb3399e87
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2025-12-30 22:44:56 +0530

    Create Break and continue.py
    Made with break and continue. stops while entering zero and skips while entering negative value and prints while entering  positive value

---
+++ Break and continue.py
@@ -0,0 +1,11 @@
+#program using break and continue statements and while loop in it 
+while True:
+    i=int(input("Enter any num:"))
+    if i<0:
+        print("skipped negative value")
+        continue
+    elif i==0:
+        print("stopped loop you entered zero")
+        break
+    else:
+        print(i,"Entered positive number")    
