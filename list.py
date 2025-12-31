ecbbeebd844719536dc0fbc2fc53b56374f0059b
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2025-12-31 19:05:24 +0530

    Update list.py

--- list.py
+++ list.py
@@ -5,7 +5,7 @@ n=int(input("Enter the number of elements you want to add to the list: "))
 for i in range(n):
     element=int(input("Enter element {}: ".format(i+1)))
     Numbers.append(element)
-print("The complete list is:", Numbers)
-print("The length of the list is:", len(Numbers))
-print("The sum of the list elements is:", sum(Numbers))
+print("The complete list is:", Numbers)                         #displaying the list
+print("The length of the list is:", len(Numbers))               #displaying the length of the list
+print("The sum of the list elements is:", sum(Numbers))         #displaying the sum of the list elements
 
