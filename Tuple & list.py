0f520dc226f18b673c319ffb97266359ff6799a8
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-01 16:21:01 +0530

    Create Tuple & list.py
    Made an program in two  types which converts list into tuples and prints the first and last element of it.

---
+++ Tuple & list.py
@@ -0,0 +1,21 @@
+#program created using list and tuple 
+#converting an list to tuple form
+
+nums=[10,20,30,40,50]
+print(nums)
+t=(type(nums))
+print(t)
+print(nums[0])
+print(nums[-1])
+nums=(10,20,30,40,50)
+print(nums)
+t1=(type(nums))
+print(t1)
+print(nums[0])
+print(nums[-1])
+#converting an list to tuple form
+
+nums=[10,20,30,40,50]
+t=tuple(nums)
+print(t)
+print(type(t))
