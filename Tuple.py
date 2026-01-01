6357726b0321ed725fb21bedf5df9de5e8207d22
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-01 15:55:51 +0530

    Update Tuple.py

--- Tuple.py
+++ Tuple.py
@@ -1,8 +1,8 @@
 #program created by using tuple
 
 t=("python","java","html","css","ruby")         #tuple cannot be changedthry are (immutable)
-print(type(t))
+print(type(t))          #shows the type
 print(t)
-print(len(t))
-print(t[0])
-print(t[0:-1])
+print(len(t))           #shows the length
+print(t[0])             #prints the first element from the tuple
+print(t[0:-1])          #prints the last element from the tuple
