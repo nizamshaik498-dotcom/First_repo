da5de4c69c9ed6bdb1fca7cc7696d9fdeac3eb74
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-02 19:42:24 +0530

    Update Tuple methods.py

--- Tuple methods.py
+++ Tuple methods.py
@@ -1,7 +1,7 @@
 #program using tuple methods and f-string
 
 nums=(10,20,30,20,40,20,50)
-t=nums.count(20)
-t1=nums.index(20)
-print(f"20 appeared {t}times.")
-print(f"first occurence of 20 is:{t1}")
+t=nums.count(20)                #used count() method
+t1=nums.index(20)               #used index() method    
+print(f"20 appeared {t}times.")                         #used f-string
+print(f"first occurence of 20 is:{t1}")                 #used f-string
