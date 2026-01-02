3cf93f1db384de432fb28a2b9302cf560bfa1770
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-02 20:00:27 +0530

    Update F-strings.py

--- F-strings.py
+++ F-strings.py
@@ -1,9 +1,9 @@
 #program created by using f-strings and take input from user and print the output
 
-name=input("Enter your name: ")
-age=int(input("Enter your age: "))
-city=input("Enter the city name: ")
-college=input("Enter college name: ")
-cgpa=float(input("Enter your marks: "))
+name=input("Enter your name: ")               #taking name as input from user
+age=int(input("Enter your age: "))            #taking age as input from user
+city=input("Enter the city name: ")           #taking city name as input from user
+college=input("Enter college name: ")         #taking college name as input from user
+cgpa=float(input("Enter your marks: "))       #taking cgpa as input from user
 
 print(f"Hey my name is {name},and iam {age} years old.and i am currently staying in {city},studying in {college} college,and i got {cgpa} in my semesters")
