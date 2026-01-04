8d44516338cea1a92ba3400395648d6500a2385f
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-04 19:01:10 +0530

    Update Sets 1.py

--- Sets 1.py
+++ Sets 1.py
@@ -1,4 +1,4 @@
-#program made by using list 
+#program made by using lists and sets to demonstrate the use of 'in' operator
 id_list={101,102,103,104}                   #created a set of id numbers    
 id_no=int(input("Enter your id number: "))
 if (id_no in id_list):                          #used in operator to check whether the id number is present in the set or not
