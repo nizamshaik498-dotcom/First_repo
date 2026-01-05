fd69d22d2e2ed404509836ea15a504ff184e2dee
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-05 20:02:26 +0530

    Create Dictionary.py
    Made by using dictionary which prints the sum and average of the subjects present in the exsisting dictionary.

---
+++ Dictionary.py
@@ -0,0 +1,19 @@
+#program created by using dictonary and printing the sum and average of the values in the dictionary present
+
+Marks={"English":95,
+       "Sanskrit":92,
+       "Maths":90,
+       "Physics":72,
+       "Computers":89}
+print(Marks["English"])
+print(Marks["Sanskrit"])
+print(Marks["Maths"])
+print(Marks["Physics"])
+print(Marks["Computers"])
+
+add=(95+92+90+72+89)
+print("Total Marks: ",add)
+avg=(95+92+90+72+89/5)
+print("Average: ",avg)
+
+
