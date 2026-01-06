3826cbb2067da4de46365bfe31216673c0cdcb27
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-06 20:31:14 +0530

    Update For loop with else.py

--- For loop with else.py
+++ For loop with else.py
@@ -2,10 +2,10 @@
 
 numbers=[10,20,30,40,50]
 find=int(input("Enter any value: "))
-for n in numbers:
+for n in numbers:                           #used for loop 
     if n==find:
         print("Number found")
         break
-    else:
+    else:                                   #used else condition with for loop
         print("Number not found")
         break
