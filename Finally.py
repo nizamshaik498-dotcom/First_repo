93e291b10cf14ad265595c3303dcb3c993a25cf0
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-07 19:48:13 +0530

    Create Finally.py
    Made by using exception handling statements and added finally statement in this.

---
+++ Finally.py
@@ -0,0 +1,20 @@
+#program made by using exception handling statements and using (finally) block in it
+
+print("A mini calculator which performs division operation:")
+
+try:
+    num1=int(input("Enter first value: "))
+    num2=int(input("Enter second value: "))
+    result=(num1/num2)
+    print("Your answer is:",result)
+except ZeroDivisionError:
+    print("Sorry! cant divide with zero")
+except ValueError:
+    print("please enter value in integer type")
+finally:
+    print("Thank you for using this mini divisable calculator")
+    
+
+
+
+    
