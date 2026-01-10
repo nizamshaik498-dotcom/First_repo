a657e7642b7fa29c792cca570d98b99ecf50291b
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-10 19:19:56 +0530

    Create Enumarate function.py
    Made by using enumerate function and printing the index number and values stored in the list as output and used start keyword and assigned (1) to it so it prints the index from(1) rather than(0).

---
+++ Enumarate function.py
@@ -0,0 +1,5 @@
+#Program created using enumarate function which shows both index number and value stored in the list
+
+Fruits=["Apple","Mango","Guava","Pine apple","Orange","Melon","Kiwi"]
+for index,value in enumerate(Fruits ,start=1):
+    print(index,value)
