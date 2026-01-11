b5a320f25e19b3ccdcbbec54ecd13b60b9fcb938
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-11 20:23:34 +0530

    Update list 1.py

--- list 1.py
+++ list 1.py
@@ -1,5 +1,5 @@
 #program made using a list and printing ithe values in the list using for loop
 
-course=["python","java","c"]
+course=["python","java","c"]                    #used list to store values
 for lang in course:
     print("Language:",lang)
