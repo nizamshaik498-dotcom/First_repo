878f26b42592eb788a84563b5770ec6fb0741d8e
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-12 20:49:50 +0530

    Update list 2.py

--- list 2.py
+++ list 2.py
@@ -1,5 +1,5 @@
 #Program created using list and using for loop to print the product of the values stored in the list
 
 num=[1,2,3,4,5]             #used list to store values
-for value in num:
+for value in num:           #used for loop to iterate through the list
     print(value*2)
