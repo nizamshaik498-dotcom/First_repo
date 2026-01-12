af426926d7dae1b62257edee030eb564de9df796
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-12 20:58:36 +0530

    Update list 3.py

--- list 3.py
+++ list 3.py
@@ -1,5 +1,5 @@
 #Program created using list which prints the sqaures of the values stored in the list
 
-num=[1,2,3,4,5]
+num=[1,2,3,4,5]                                     #used of list to store values
 for calc in num:
     print("Number:",calc,"Square:",calc**2)
