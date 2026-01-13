3620f497a46f1d91949e94558306b8c371ede8b8
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-13 20:42:44 +0530

    Update For loop3.py

--- For loop3.py
+++ For loop3.py
@@ -3,7 +3,7 @@
 num=[1,2,3,4,5]
 for even in num:
     if even %2==0:                                  #used if condition to check even numbers
-        print("These are even num:",even)
+        print("These are even num:",even)           #prints only even numbers
         
 
 #program using for loop which prints only odd numbers from the values which are stored in the list
