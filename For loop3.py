b1613fcb1f138b8f1ec5fe9f18a410749f7523fd
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-13 20:40:32 +0530

    Create For loop3.py
    Made using for loop and values stored in list which prints even and odd numbers from the list

---
+++ For loop3.py
@@ -0,0 +1,13 @@
+#Program using for loop which prints only even numbers from the values which are stored in the list
+
+num=[1,2,3,4,5]
+for even in num:
+    if even %2==0:
+        print("These are even num:",even)
+        
+
+#program using for loop which prints only odd numbers from the values which are stored in the list
+num=[1,2,3,4,5]
+for odd in num:
+    if odd %2!=0:
+        print("These are odd num:",odd)
