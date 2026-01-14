48370eb9486f6856065ee424cc0d3b4bf6d0a052
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-14 22:39:08 +0530

    Update Count num.py

--- Count num.py
+++ Count num.py
@@ -1,8 +1,8 @@
 #Program created using list and for loop as well as if, else statement
 
 nums=[1,2,3,4,5,6,7,8,9,10]
-Even_count=0
-Odd_count=0
+Even_count=0                            #used even count variable to count even numbers
+Odd_count=0                             #used odd count variable to count odd numbers
 for n in nums:
     if n%2==0:
         Even_count +=1
