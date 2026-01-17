860382bb3a08897f1c67bc348bfc8e1025176507
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-17 20:16:50 +0530

    Create Phone Lock.py
    Made program using while loop ,if, elif, else statements. Which gives 3 attempts to user to unlock his phone if attempts are completed it will print Phone locked.

---
+++ Phone Lock.py
@@ -0,0 +1,16 @@
+#program created using while loop,if, elif, else statements (Phone Lock).
+
+password=4321
+attempts=3
+
+while attempts>0:
+    pin=int(input("Enter PIN Number: "))
+    if pin==password:
+        print("Phone Unlocked")
+        break
+    else:
+        attempts= attempts-1
+        print("Wrong PIN!")
+        print("Attempts Remaining: ",attempts)
+    if attempts==0:
+        print("Phone Locked!!!")
