0ff72cf696d441b46df372f12a321dbe6e4e4557
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-25 21:22:21 +0530

    Create File handling.py
    Made using file handling functions takes names input from user and save it in the file

---
+++ File handling.py
@@ -0,0 +1,9 @@
+#program created using file handling functions
+
+username=input("Enter username: ")
+
+with open("users.txt","a") as file:
+    file.write(username+"\n")
+print("\n Saved usernames: ")
+with open("users.txt","r") as file:
+    print(file.read())
