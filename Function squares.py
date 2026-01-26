0d9b7aef35663dfc7bdf4e8ed29696a1490a7b64
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-26 21:25:39 +0530

    Create Function squares.py
    Made by using  function which takes input from user and prints the square of both by adding them

---
+++ Function squares.py
@@ -0,0 +1,9 @@
+#program created using functions and prints the square value from the numbers given as input from the user.
+
+val1=int(input("Enter 1st number: "))
+val2=int(input("Enter 2nd number: "))
+def value():
+    num=val1*val1+val2*val2
+    return num
+out=value()
+print("Output: ",out) 
