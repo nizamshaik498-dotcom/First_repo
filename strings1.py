6416b27b92906bef057e761d89ac8d3d49f728fc
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-27 20:57:19 +0530

    Update strings1.py

--- strings1.py
+++ strings1.py
@@ -3,7 +3,7 @@
 prompt=input("Enter any sentence: ")                        #takes input from user
 print("Your Input Is: ",prompt)
 print("Coverted to title case: ",prompt.title())            #coverts to title case
-vowels=(prompt.lower().count('a')+
+vowels=(prompt.lower().count('a')+                          #counts vowels in the input
         prompt.lower().count('e')+
         prompt.lower().count('i')+
         prompt.lower().count('o')+
