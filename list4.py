49b38a726162a00e58e354ac3feb7ff76c71535c
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-29 21:02:57 +0530

    Update list4.py

--- list4.py
+++ list4.py
@@ -2,7 +2,7 @@
 
 num=list(map(int,input("Enter numbers: ").split())) #takes input from user and store in the list
 largest=max(num)                                    #finds the largest number in the list
-smallest=min(num)
+smallest=min(num)                                   #finds the smallest number in the list
 average=sum(num)/len(num)
 print("The largest number is: ",largest)
 print("The smallest number is: ",smallest)
