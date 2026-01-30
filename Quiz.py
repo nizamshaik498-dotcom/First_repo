2a6ebebd17b1db1a34ccb6bcb3baf6fbde4afe9e
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-30 21:04:49 +0530

    Create Quiz.py
    Made by using if statement asks questions for user and takes answers as input from them and prints the score  of the right answers.

---
+++ Quiz.py
@@ -0,0 +1,13 @@
+#program created using if else statements and ask the user to enter the answers for the questions.
+
+score=0
+ans1=input("Who Is The CEO Of Tesla/X/Spacex: ")
+ans2=input("What Is The Abbrevation Of CEO: ")
+ans3=input("What Is Trade ?: ")
+if ans1.lower()=="elon musk":
+    score+=1
+if ans2.lower()=="chief executive officer":
+    score+=1
+if ans3.lower()=="buying and selling":
+    score+=1
+print("Your score is : ",score)
