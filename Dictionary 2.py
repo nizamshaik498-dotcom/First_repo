5213fedeb7f5df6d821ed251495e3b23bc25059b
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-31 21:05:38 +0530

    Update Dictionary 2.py

--- Dictionary 2.py
+++ Dictionary 2.py
@@ -1,6 +1,6 @@
 #program created using dictionary to store contacts
 name=input("Enter name: ")                                                          #input name to search in contacts
-contacts={"nizam":154251,"althaf":567423,"wahid":654786,"mike":367399}
+contacts={"nizam":154251,"althaf":567423,"wahid":654786,"mike":367399}              #dictionary to store contacts
 name=name.lower()
 if name in contacts:
     print("The mobile number is: ",contacts[name])
