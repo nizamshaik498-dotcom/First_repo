620f3af00a72fe1c3293d99e28dcac49dc643112
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-02-02 20:42:03 +0530

    Create oops 1.py
    Made program using constructor (__init__) with the help of class and object.(oops)

---
+++ oops 1.py
@@ -0,0 +1,11 @@
+#program created using class and object (oops) using a constructor (__init__ method)
+
+class student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def display(self):
+        print("My name is: ",self.name)
+        print("My age is: ",self.age)
+stud=student("Max",20)
+stud.display()
