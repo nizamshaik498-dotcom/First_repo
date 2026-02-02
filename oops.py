b9432df6a91dcd8224c0099e0a4fb943b4a8cb97
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-02-02 20:31:47 +0530

    Update oops.py

--- oops.py
+++ oops.py
@@ -4,7 +4,7 @@ class student:                                             #class name
     def get(self,name,age):                                #method to get data
         self.name=name
         self.age=age
-    def display(self):
+    def display(self):                                     #method to display data                 
         print("My name is :",self.name)
         print("My age is: ",self.age)
 stud1=student()
