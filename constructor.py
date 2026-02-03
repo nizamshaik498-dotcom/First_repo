2af099d4d0bb496b9a14179d4b8f3ce8bace4f26
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-02-04 04:23:53 +0530

    Create constructor.py
    Made a program using constructor (parameterized-constructor)

---
+++ constructor.py
@@ -0,0 +1,14 @@
+#program created using constructor(parameterized).
+
+class Student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+        print("This is parameterized constructor")
+    def display(self):
+        print("My name is: ",self.name)
+        print("My age is: ",self.age)
+student=Student("Max",30)
+student.display()
+
+
