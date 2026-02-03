3cfb5d8fa2ee27eb81c05c3e37cfd2c498fe0f3a
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-02-04 04:25:10 +0530

    Update constructor.py

--- constructor.py
+++ constructor.py
@@ -5,7 +5,7 @@ class Student:                                              #class name
         self.name=name
         self.age=age
         print("This is parameterized constructor")
-    def display(self):
+    def display(self):                                      #method to display                               
         print("My name is: ",self.name)
         print("My age is: ",self.age)
 student=Student("Max",30)
