5d33d3620feb980c091862a0eef8cd94531e1ba3
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-02-05 20:48:34 +0530

    Create multilevel inheritance.py
    Made a program by using multilevel inheritance  .

---
+++ multilevel inheritance.py
@@ -0,0 +1,11 @@
+#program created using multilevel inheritance
+
+class Animal:                           
+    def speak(self):                   
+        print("Animal Sound")
+class Dog(Animal):                                 
+    def bark(self):                     
+        print("Barking sound")
+d=Dog()
+d.speak()
+d.bark()
